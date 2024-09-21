0f70a8d89e9d562ad687833b05e0147ab4f27ee4
Marija Dimovska <marija-dimoska@live.com>
2024-09-21 17:06:56 +0200

    Update firebaseConfig.js

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,17 +1,1 @@
-// clientApp.ts
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
-};
-
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-
-export { firestore };
